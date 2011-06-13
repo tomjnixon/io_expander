a02a0a53e510e437a9bcfbd9b54492c61d5e283e
Thomas Nixon <tom@tomn.co.uk>
2011-06-13 21:37:48 +0100

    maybe working...

--- i2c.c
+++ i2c.c
@@ -1,13 +1,15 @@
 #define __16f876a
 #include "pic16f876a.h"
 
+#include "commands.h"
+
 /* Setup chip configuration */
 typedef unsigned int config;
 config __at 0x2007 __CONFIG = _CP_OFF & 
 	_WDT_OFF & 
 	_BODEN_OFF & 
 	_PWRTE_OFF & 
-	_XT_OSC &
+	_RC_OSC &
 	_LVP_OFF;
 
 unsigned char int_c = 0;
@@ -59,51 +61,53 @@ void handle_command(unsigned char command)
 	switch(state) {
 		case 0:
 			switch(command) {
-				case 0:
-				case 1:
-				case 2:
-				case 3:
-				case 4:
+				case CMD_READ_AN0:
+				case CMD_READ_AN1:
+				case CMD_READ_AN2:
+				case CMD_READ_AN3:
+				case CMD_READ_AN4:
 					analogue_read(command);
 					break;
-				case 5:
+				case CMD_READ_PORTC:
 					buffer[0] = PORTC;
 					buffer_size = 1;
 					portb = portb & 0x7F;
 					PORTB = portb;
 					break;
-				case 6:
+				case CMD_READ_PORTB:
 					buffer[0] = PORTB;
 					buffer_size = 1;
 					portb = portb & 0x7F;
 					PORTB = portb;
 					break;
-				case 7:
-				case 8:
-				case 9:
-				case 10:
-				case 11:
-				case 12:
+				case CMD_SET_TRISC:
+				case CMD_SET_TRISB:
+				case CMD_SET_PORTC:
+				case CMD_SET_PORTB:
+				case CMD_SET_INTC:
+				case CMD_SET_INTB:
 					state = command;
 			}
 			break;
-		case 7:
-			TRISC = command;
+		case CMD_SET_TRISC:
+			// RC3 & RC4 are used for I2C, and must be outputs.
+			TRISC = command | 0b00011000;
 			break;
-		case 8:
+		case CMD_SET_TRISB:
+			// The interrupt out pin in RB7.
 			TRISB = command & 0x7F;
 			break;
-		case 9:
+		case CMD_SET_PORTC:
 			PORTC = command;
 			break;
-		case 10:
+		case CMD_SET_PORTB:
 			portb = command;
 			PORTB = portb;
 			break;
-		case 11:
+		case CMD_SET_INTC:
 			int_c = command;
 			break;
-		case 12:
+		case CMD_SET_INTB:
 			int_b = command;
 			break;
 	}
@@ -115,20 +119,40 @@ static void isr(void) __interrupt 0 {
 	unsigned char buf;
 	if (SSPIF) {
 
+		buf = SSPBUF;
+		address = SSPBUF;
+
 		if (I2C_READ) {
-			SSPBUF = buffer_pop();
+			if (buffer_size)
+				SSPBUF = buffer_pop();
+			if (SSPOV) {
+				TRISB = 0x7E;
+				PORTB = 1;
+			}
 		} else {
 			// process command
-			
-			buf = SSPBUF;
-			if (I2C_DATA)
+			if (I2C_DATA) {
 				handle_command(buf);
+				if (SSPOV) {
+					TRISB = 0x7D;
+					PORTB = 2;
+				}
+			}
 			else {
+				__asm
+					MOVF	_SSPBUF, W
+				__endasm;
 				address = buf;
 				state = 0;
+				if (SSPOV) {
+					TRISB = 0x7B;
+					PORTB = 4;
+				}
 			}
-			BF = 0;
+		BF = 0;
 		}
+
+
 
 		SSPIF = 0;
 		CKP = 1;
@@ -139,6 +163,7 @@ void main(void) {
 
 	TRISA = 0xFF; 
 	TRISB = 0x7F;
+	NOT_RBPU = 0;
 	portb = 0;
 	PORTB = portb;
 	TRISC = 0xFF;
@@ -166,13 +191,18 @@ void main(void) {
 	GIE = 1;                /* global interrupt enable */
 
 	while (1) {
-		if (((last_c ^ PORTC) & int_c) || ((last_b ^ PORTB) & int_b)) {
-			portb = portb | 0x80;
-			PORTB = portb;
-		
-			last_c = PORTC;
-			last_b = PORTB;
+		if (SSPOV) {
+			TRISB = 0;
+			PORTB = SSPBUF;
+			while (1) {}
 		}
+		// if (((last_c ^ PORTC) & int_c) || ((last_b ^ PORTB) & int_b)) {
+		// 	portb = portb | 0x80;
+		// 	PORTB = portb;
+		// 
+		// 	last_c = PORTC;
+		// 	last_b = PORTB;
+		// }
 	}
 	
 }
