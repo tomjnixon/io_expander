2ed70bb8f0f3102d6fff5a2e491271e6a94e34d0
Thomas Nixon <tom@tomn.co.uk>
2011-06-13 23:31:28 +0100

    Command definition now in a seperate header.

---
+++ commands.h
@@ -0,0 +1,18 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define CMD_READ_AN0       0
+#define CMD_READ_AN1       1
+#define CMD_READ_AN2       2
+#define CMD_READ_AN3       3
+#define CMD_READ_AN4       4
+#define CMD_READ_PORTC     5
+#define CMD_READ_PORTB     6
+#define CMD_SET_TRISC      7
+#define CMD_SET_TRISB      8
+#define CMD_SET_PORTC      9
+#define CMD_SET_PORTB      10
+#define CMD_SET_INTC       11
+#define CMD_SET_INTB       12
+
+#endif
